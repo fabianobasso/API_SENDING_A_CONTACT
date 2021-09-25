284648a567b0e5dd2a84c4091a4b184512df6c86
Fabiano Basso <31233012+fabianobasso@users.noreply.github.com>
2021-09-25 17:53:32 -0300

    Update server.js
    Change Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -7,10 +7,17 @@ require('dotenv').config()
 /**
  *  Config app
  */
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`
         http://localhost:${port}
     `)
