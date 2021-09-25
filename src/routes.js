535dee9fb3a8a876971c293f28edc3a688fb40bd
Fabiano Basso <31233012+fabianobasso@users.noreply.github.com>
2021-09-25 17:59:45 -0300

    change route send_mail for send_contact
    change route send_mail for send_contact

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const route = express.Router()
 const sendController = require('./controllers/SendMailController')
 
-route.post('/send_mail', sendController.SendMail)
+route.post('/send_contact', sendController.SendMail)
 route.get('/', sendController.HomeMail)
 
 
